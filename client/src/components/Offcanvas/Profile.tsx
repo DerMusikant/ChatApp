3ef6c61bbe14ad97dfa5ca8671963b5dac95a5cd
DerMusikant <rivettivalentin@gmail.com>
2022-05-30 20:13:33 -0300

    Finished confirm on delete, to do: chat functionality

--- client/src/components/Offcanvas/Profile.tsx
+++ client/src/components/Offcanvas/Profile.tsx
@@ -1,17 +1,24 @@
 import { User } from '../../types/UserTypes'
+
+import Alert from 'react-bootstrap/Alert'
 import Offcanvas from 'react-bootstrap/Offcanvas'
 import Button from 'react-bootstrap/Button'
+
 import React from 'react'
 
 type Props = {
     user: User,
     showProfile: boolean,
+    showDeleteAccount: boolean,
     logOut: () => void,
     deleteAccount: () => void,
     handleCloseProfile: () => void,
+    setShowDelete: (show: boolean) => void,
+    reference: (ref: object) => void
 }
 
-export const Profile: React.FC<Props> = ({ user, logOut, handleCloseProfile, showProfile, deleteAccount }) => {
+export const Profile: React.FC<Props> = ({ user, logOut, handleCloseProfile, showProfile, showDeleteAccount, setShowDelete, deleteAccount, reference }) => {
+
     return (
         <>
             <Offcanvas show={showProfile} onHide={handleCloseProfile}>
@@ -31,9 +38,25 @@ export const Profile: React.FC<Props> = ({ user, logOut, handleCloseProfile, sho
                     <Button variant="outline-warning" onClick={logOut}>
                         Log out
                     </Button>
-                    <Button variant="outline-danger" onClick={deleteAccount}>
-                        Delete Account
-                    </Button>
+                    {
+                        showDeleteAccount ?
+                            <div ref={reference}>
+                                <Alert variant='danger' onClose={() => setShowDelete(false)} dismissible>
+                                    <Alert.Heading>Are you sure?</Alert.Heading>
+                                    <p>
+                                        Deleting an account is forever! You won't find it ever again...
+                                    </p>
+                                    <Button variant="outline-danger" onClick={deleteAccount}>
+                                        Delete Account
+                                    </Button>
+                                </Alert>
+                            </div>
+                            :
+                            <Button variant="outline-danger" onClick={() => {setShowDelete(true)}}>
+                                Delete Account
+                            </Button>
+                    }
+
                 </Offcanvas.Body>
             </Offcanvas>
         </>
